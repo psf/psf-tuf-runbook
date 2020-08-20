9b2d6ba23076b8277539cbcc3b04eee8238cb41e
William Woodruff <william@trailofbits.com>
2020-08-20 17:09:48 -0400

    nitrohsm-provision: Drop all key generation
    This was broken for reasons that are not worth our time to
    investigate, probably something to do with structure alignment
    or padding at the PKCS11 layer. We'll use `pkcs11-tool` for
    key generation instead.

--- nitrohsm-provision/src/main.rs
+++ nitrohsm-provision/src/main.rs
@@ -5,24 +5,8 @@ use pkcs11::{types, Ctx};
 use rand::Rng;
 use regex::Regex;
 
-use std::fs::File;
-use std::io::Write;
 use std::path::Path;
 use std::process;
-
-type PubkeyAttrs = [types::CK_ATTRIBUTE; 10];
-
-type PrivkeyAttrs = [types::CK_ATTRIBUTE; 11];
-
-type EccParams = (types::CK_MECHANISM, PubkeyAttrs, PrivkeyAttrs);
-
-// The suffix for the file that we'll write the root keypair's public key to.
-// This will have the same ultimate path format as the internal attestation path.
-const TUF_ROOT_KEY_PUBKEY_FILE_SUFFIX: &'static str = "root_pubkey.pub";
-
-// The suffix for the file that we'll write the targets keypair's public key to.
-// This will have the same ultimate path format as the internal attestation path.
-const TUF_TARGETS_KEY_PUBKEY_FILE_SUFFIX: &'static str = "targets_pubkey.pub";
 
 const OPENSC_PKCS11_SO: &'static str = "/usr/local/lib/opensc-pkcs11.so";
 
@@ -53,11 +37,6 @@ const SO_PIN_LENGTH: usize = 16;
 // We use 6 characters chosen from the lowercase alphabet + numbers.
 const USER_PIN_ALPHABET: &'static [u8] = b"abcdefghijklmnopqrstuvwxyz0123456789";
 const USER_PIN_LENGTH: usize = 6;
-
-// The DER-encoded OIDs for our supported key types.
-const P256_OID: &'static [types::CK_BYTE] =
-    &[0x06, 0x08, 0x2A, 0x86, 0x48, 0xCE, 0x3D, 0x03, 0x01, 0x07];
-const P384_OID: &'static [types::CK_BYTE] = &[0x06, 0x05, 0x2B, 0x81, 0x04, 0x00, 0x22];
 
 fn confirm(msg: &str) -> Result<(), String> {
     match Confirmation::new().with_text(msg).default(false).interact() {
@@ -335,174 +314,10 @@ fn perform_factory_reset(
     Ok(new_user_pin)
 }
 
-fn get_ecc_keypair_params(key_type: &str, label: &str) -> Result<EccParams, String> {
-    let mechanism = types::CK_MECHANISM {
-        mechanism: types::CKM_EC_KEY_PAIR_GEN,
-        pParameter: std::ptr::null_mut(),
-        ulParameterLen: 0,
-    };
-
-    let public_key_class = types::CKO_PUBLIC_KEY;
-    let private_key_class = types::CKO_PRIVATE_KEY;
-    let ec_key_type = types::CKK_EC;
-    let b_true = types::CK_TRUE;
-    let b_false = types::CK_FALSE;
-
-    let key_type_oid = match key_type {
-        "p256" => P256_OID,
-        "p384" => P384_OID,
-        _ => unreachable!(),
-    };
-
-    let pubkey_template: PubkeyAttrs = [
-        // This is a public key.
-        types::CK_ATTRIBUTE::new(types::CKA_CLASS).with_ck_ulong(&public_key_class),
-        // PKCS#11 v2.40-CS01 S 2.3.5: We specify the CKA_EC_PARAMS for the entire
-        // keypair using an attribute on the public key.
-        // http://docs.oasis-open.org/pkcs11/pkcs11-curr/v2.40/cs01/pkcs11-curr-v2.40-cs01.html#_Toc399398881
-        types::CK_ATTRIBUTE::new(types::CKA_EC_PARAMS).with_bytes(&key_type_oid),
-        // The keypair's label.
-        types::CK_ATTRIBUTE::new(types::CKA_LABEL).with_string(format!("{}-pub", label).as_str()),
-        // The keypair's "type". This is really the general class of keytype, in this case CKA_EC.
-        types::CK_ATTRIBUTE::new(types::CKA_KEY_TYPE).with_ck_ulong(&ec_key_type),
-        // Our public key supports verification where the signature is an appendix to the data.
-        // TODO: Do we need this?
-        types::CK_ATTRIBUTE::new(types::CKA_VERIFY).with_bool(&b_true),
-        // Our public key doesn't support encryption.
-        types::CK_ATTRIBUTE::new(types::CKA_ENCRYPT).with_bool(&b_false),
-        // Our public key doesn't support key wrapping.
-        types::CK_ATTRIBUTE::new(types::CKA_WRAP).with_bool(&b_false),
-        // Our public key is a token object (i.e., not ephemeral with the session).
-        types::CK_ATTRIBUTE::new(types::CKA_TOKEN).with_bool(&b_true),
-        // Our public key is HSM-private; a user needs to be authenticated to retrieve it.
-        types::CK_ATTRIBUTE::new(types::CKA_PRIVATE).with_bool(&b_true),
-        // Our public key is not modifiable (e.g., no label changes).
-        types::CK_ATTRIBUTE::new(types::CKA_MODIFIABLE).with_bool(&b_false),
-    ];
-
-    let privkey_template: PrivkeyAttrs = [
-        // This is a private key.
-        types::CK_ATTRIBUTE::new(types::CKA_CLASS).with_ck_ulong(&private_key_class),
-        // Like above; the keypair's label.
-        types::CK_ATTRIBUTE::new(types::CKA_LABEL).with_string(format!("{}-priv", label).as_str()),
-        // Like above; the keypair's "type".
-        types::CK_ATTRIBUTE::new(types::CKA_KEY_TYPE).with_ck_ulong(&ec_key_type),
-        // Our private key supports signatures where the signature is an appendix to the data.
-        // TODO: Do we need this?
-        types::CK_ATTRIBUTE::new(types::CKA_SIGN).with_bool(&b_true),
-        // Our private key does not support decryption.
-        types::CK_ATTRIBUTE::new(types::CKA_DECRYPT).with_bool(&b_false),
-        // Our private key does not support unwrapping.
-        types::CK_ATTRIBUTE::new(types::CKA_UNWRAP).with_bool(&b_false),
-        // Our private key is "sensitive"; not all of its attributes are available.
-        types::CK_ATTRIBUTE::new(types::CKA_SENSITIVE).with_bool(&b_true),
-        // Our private key is a token object (i.e., not ephemeral with the session).
-        types::CK_ATTRIBUTE::new(types::CKA_TOKEN).with_bool(&b_true),
-        // Our private key is HSM-private.
-        // TODO: What does this mean in the context of a private key?
-        types::CK_ATTRIBUTE::new(types::CKA_PRIVATE).with_bool(&b_true),
-        // Our private key is not extractable from the HSM.
-        types::CK_ATTRIBUTE::new(types::CKA_EXTRACTABLE).with_bool(&b_false),
-        // Our private key is not modifiable (e.g., no label changes).
-        types::CK_ATTRIBUTE::new(types::CKA_MODIFIABLE).with_bool(&b_false),
-    ];
-
-    Ok((mechanism, pubkey_template, privkey_template))
-}
-
-fn new_ecc_keypair(
-    pkcs11_ctx: &Ctx,
-    slot: types::CK_SLOT_ID,
-    user_pin: &str,
-    ecc_params: &EccParams,
-) -> Result<Vec<types::CK_BYTE>, String> {
-    let (mechanism, pubkey_template, privkey_template) = ecc_params;
-
-    let session = match pkcs11_ctx.open_session(
-        slot,
-        types::CKF_SERIAL_SESSION | types::CKF_RW_SESSION,
-        None,
-        None,
-    ) {
-        Ok(session) => session,
-        Err(e) => return Err(format!("failed to open session with HSM: {}", e)),
-    };
-
-    if let Err(e) = pkcs11_ctx.login(session, types::CKU_USER, Some(user_pin)) {
-        pkcs11_ctx
-            .close_session(session)
-            .unwrap_or_else(|e| eprintln!("Error while closing session: {}", e));
-        return Err(format!("failed to login: {}", e));
-    }
-
-    let (pubkey_handle, privkey_handle) =
-        match pkcs11_ctx.generate_key_pair(session, mechanism, pubkey_template, privkey_template) {
-            Ok((pubkey_handle, privkey_handle)) => (pubkey_handle, privkey_handle),
-            Err(e) => {
-                pkcs11_ctx
-                    .close_session(session)
-                    .unwrap_or_else(|e| eprintln!("Error while closing session: {}", e));
-                return Err(format!("failed to generate keypair: {}", e));
-            }
-        };
-
-    // Using the handle for the newly created public key, grab the CKA_EC_POINT
-    // (i.e., the public key).
-    // The PKCS#11 API is a little clumsy here: we call get_attribute_value once
-    // to retrieve the required size, then call it again with that space allocated
-    // to get the actual value.
-    let mut ec_point_template = vec![types::CK_ATTRIBUTE::new(types::CKA_EC_POINT)];
-    let ec_point_buf =
-        match pkcs11_ctx.get_attribute_value(session, pubkey_handle, &mut ec_point_template) {
-            Ok((types::CKR_ATTRIBUTE_TYPE_INVALID, _)) | Err(_) => {
-                pkcs11_ctx
-                    .close_session(session)
-                    .unwrap_or_else(|e| eprintln!("Error while closing session: {}", e));
-                return Err(format!("failed to get CKA_EC_POINT length for pubkey"));
-            }
-            Ok((_, _)) => {
-                Vec::<types::CK_BYTE>::with_capacity(ec_point_template[0].ulValueLen as usize)
-            }
-        };
-    ec_point_template[0].set_bytes(&ec_point_buf.as_slice());
-
-    match pkcs11_ctx.get_attribute_value(session, pubkey_handle, &mut ec_point_template) {
-        Ok((types::CKR_OK, _)) => {}
-        Ok((e, _)) => {
-            pkcs11_ctx
-                .close_session(session)
-                .unwrap_or_else(|e| eprintln!("Error while closing session: {}", e));
-            return Err(format!("failed to populate CKA_EC_POINT buffer: {}", e));
-        }
-        Err(e) => {
-            pkcs11_ctx
-                .close_session(session)
-                .unwrap_or_else(|e| eprintln!("Error while closing session: {}", e));
-            return Err(format!("failed to get CKA_EC_POINT: {}", e));
-        }
-    };
-
-    if let Err(e) = pkcs11_ctx.close_session(session) {
-        return Err(format!("Failed to close session: {}", e));
-    }
-
-    Ok(ec_point_buf)
-}
-
 fn run() -> Result<(), String> {
     let matches = App::new(env!("CARGO_PKG_NAME"))
         .version(env!("CARGO_PKG_VERSION"))
         .about(env!("CARGO_PKG_DESCRIPTION"))
-        .arg(
-            Arg::with_name("type")
-                .help("sets the ecc key type")
-                .short("t")
-                .long("type")
-                .multiple(false)
-                .takes_value(true)
-                .possible_values(&["p256", "p384"])
-                .required(true),
-        )
         .arg(
             Arg::with_name("so-pin")
                 .help("the current Security Officer PIN")
@@ -516,50 +331,16 @@ fn run() -> Result<(), String> {
         .get_matches();
 
     let so_pin = matches.value_of("so-pin").unwrap();
-    let key_type = matches.value_of("type").unwrap();
 
     big_scary_banner()?;
 
-    let (pkcs11_ctx, slot, serial_number) = find_hsm()?;
-
-    // TODO: Create an HSM session here, instead of passing the context and slot
-    // around all over the place.
-
-    // file presence checks
+    let (pkcs11_ctx, slot, _) = find_hsm()?;
 
     // Step 1: Ensure that the Nitrokey is in an acceptable state. This includes:
     //  1. Reinitializing the HSM using the current SO PIN.
     //  2. Setting a new SO PIN.
     //  3. Creating the normal user account and PIN.
-    let user_pin = perform_factory_reset(&pkcs11_ctx, slot, &so_pin)?;
-
-    // Step 2: Using the new user account, generate two keypairs
-    // suitable for signing operations.
-    let root_params = get_ecc_keypair_params(&key_type, "tuf-root")?;
-    let targets_params = get_ecc_keypair_params(&key_type, "tuf-targets")?;
-
-    let root_pubkey = new_ecc_keypair(&pkcs11_ctx, slot, &user_pin, &root_params)?;
-    let targets_pubkey = new_ecc_keypair(&pkcs11_ctx, slot, &user_pin, &targets_params)?;
-
-    for tup in vec![
-        (TUF_ROOT_KEY_PUBKEY_FILE_SUFFIX, root_pubkey),
-        (TUF_TARGETS_KEY_PUBKEY_FILE_SUFFIX, targets_pubkey),
-    ] {
-        let path = format!("{}_{}", serial_number, tup.0);
-        let mut file = match File::create(Path::new(&path)) {
-            Ok(file) => file,
-            Err(e) => {
-                return Err(format!(
-                    "attestation file creation failed: {}: {}",
-                    tup.0, e
-                ))
-            }
-        };
-
-        if let Err(e) = file.write_all(&tup.1) {
-            return Err(format!("attestation file I/O failed: {}: {}", tup.0, e));
-        }
-    }
+    perform_factory_reset(&pkcs11_ctx, slot, &so_pin)?;
 
     Ok(())
 }
